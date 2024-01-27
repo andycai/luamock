7c1efe6cd52fb4d3b2bfb331c1ea9d8d75cb8780
Andy Cai <huayicai@gmail.com>
2024-01-27 10:35:10 +0800

    Add test.lua

---
+++ test.lua
@@ -0,0 +1,21 @@
+local name = require('name')
+
+print(name.firstname())
+print(name.lastname())
+print(name.fullname(true))
+print(name.cfirstname())
+print(name.clastname())
+print(name.cfullname())
+print(name.cfullname())
+
+local base = require('base')
+
+print(base.bool())
+print(base.natural(10, 20))
+print(base.natural(-10, 200))
+print(base.int(10, 20))
+print(base.int(10, 2))
+print(base.char('all'))
+print(base.char())
+print(base.char('ab@#$%^&*cdefghijk'))
+print(base.string('ab@#$%^&*cdefghijk', 3, 6))
