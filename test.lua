530b1b5ff5cb173c51bbeec326904eb694bcb725
Andy Cai <huayicai@gmail.com>
2024-01-28 18:37:36 +0800

    Add address.lua, get random province, city, county and so on

--- test.lua
+++ test.lua
@@ -1,5 +1,4 @@
 local name = require('name')
-
 print(name.firstname())
 print(name.lastname())
 print(name.fullname(true))
@@ -9,7 +8,6 @@ print(name.cfullname())
 print(name.cfullname())
 
 local base = require('base')
-
 print(base.bool())
 print(base.natural(10, 20))
 print(base.natural(-10, 200))
@@ -19,3 +17,13 @@ print(base.char('all'))
 print(base.char())
 print(base.char('ab@#$%^&*cdefghijk'))
 print(base.string('ab@#$%^&*cdefghijk', 3, 6))
+
+local address = require('address')
+print(address.region())
+print(address.zip())
+print(address.province())
+print(address.city())
+print(address.city(true))
+print(address.county())
+print(address.county(true))
+print(address.countyId())
